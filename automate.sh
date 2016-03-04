fcdb81ebd75d8765c8f33975bdd1896f0c295f5f
Hoa Ly <lythaihoa2311992@gmail.com>
2016-03-04 14:51:47 -0800

    script to run 3 machine at the same time

---
+++ automate.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+i=0
+while read line
+do
+    array[ $i ]="$line"        
+    i=$[$i+1]
+#replace locahost by you file
+done < <(cat localhosts) 
+
+length=$[${#array[@]}-1]
+
+i=0
+while [ $i -lt $length ]
+do
+	j=0
+	while [ $j -lt 3 ]; do
+		#replace this playbook by your playbook, but the host should be {{ target }}  
+		ansible-playbook -i hosts playbook.yml -e"target=${array[i]}"
+		i=$[$i+1]
+		j=$[$j+1]
+	done
+	echo "Do you want to continue? (yes/no)"
+	read value
+	if [[ $value == "no" ]]
+	then 
+		break
+	fi
+done 
+
