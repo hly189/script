6494b452c917be64425af06b66d1e57e32b2d579
Hoa Ly <lythaihoa2311992@gmail.com>
2016-03-06 17:03:50 -0800

    delete volume atumatically

--- ec2_delete_instance.sh
+++ ec2_delete_instance.sh
@@ -16,4 +16,23 @@ aws ec2 describe-instance-status --region $REGION | grep -i "InstanceId" | cut -
 echo Which instance do you want to termninate?
 read INSTANCE_ID
 
+
+while read line
+do
+    vol[ $i ]="$line"
+    #echo ${vol[$i]}
+    i=$[$i+1]
+done < <(aws ec2 describe-instances --region us-west-2 --instance $INSTANCE_ID | awk '/VolumeId/{gsub(/[",]+/, "", $2); print $2}')
+
+
 aws ec2 terminate-instances --region $REGION --instance-ids $INSTANCE_ID
+
+sleep 120
+
+i=0
+while [ $i -lt 2 ]
+do 
+	aws ec2 delete-volume --region $REGION --volume-id ${vol[$i]}
+	#echo ${vol[$i]}
+	 i=$[$i+1]
+done 
