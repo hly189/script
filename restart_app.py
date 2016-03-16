591edd8ab89387484b27ff6f4c33f9f2192be4e5
Hoa Ly <lythaihoa2311992@gmail.com>
2016-03-16 02:28:46 -0700

    Adding options for the script

--- restart_app.py
+++ restart_app.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 
 import os
-import subprocess
-from sys import argv
-from subprocess import PIPE
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-g", 
+		   help="Machine name such as parser, spliter, writer, etc",
+		   default=False,
+		   required=True)
+parser.add_argument("-a", 
+		   help="Application name such as parserapp, spliterapp, etc",
+		   default=False,
+		   required=True)
+args = parser.parse_args()
 
-#command = os.popen("cat localhosts")
-#now = command.read()
-
-script, first, second = argv
 
 MACHINE = []
-with os.popen("cat"+" " +first) as f: 
+with os.popen("cat"+" " +args.g) as f: 
 	MACHINE=[line.split()[0] for line in f]
 
 i = 0
@@ -25,7 +29,7 @@ while i < len(MACHINE):
                 i = i+1
                 if i >= len(MACHINE)-1: break
                 j = j+1
-	a = "{}".format(": ".join(SUB_MACHINE)) + " " + second
+	a = "{}".format(":".join(SUB_MACHINE)) + " " + args.a
 	os.system("echo" + " " + a)
 	os.system("echo sleep 3 mins")
 	os.system("date")
